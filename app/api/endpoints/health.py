341f0d42c14a085a00deac30f1d5d1cd04816375
Siarhei Samoshyn <gds.grey@gmail.com>
2025-05-02 22:37:15 +0300

    update auth check

--- app/api/endpoints/health.py
+++ app/api/endpoints/health.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
-
 from app.core.responses import response
+from app.middleware.authentication import AuthenticationMiddleware
+from fastapi import Request
 
 router = APIRouter(prefix='/health', tags=['health'])
 
-@router.get("/health")
-def get_health_check():
+@router.get("/app")
+async def get_health_check():
     return response("application is running", 200, "success")
+
+@router.get("/token")
+async def check_token(request: Request):
+    await AuthenticationMiddleware.check_token(request)
+    return response("token is valid", 200, "success")
--- app/middleware/authentication.py
+++ app/middleware/authentication.py
@@ -1,3 +1,5 @@
+import logging
+
 import jwt
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -15,19 +17,25 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
         if any(path.startswith(p) for p in EXCLUDE_PATHS):
             response = await call_next(request)
             return response
+        await self.check_token(request)
+        response = await call_next(request)
+        return response
 
+    @staticmethod
+    async def check_token(request):
         auth_header = request.headers.get('Authorization')
+        logging.info(f"request.headers: {request.headers}")
+        logging.info(f"auth_header: {auth_header}")
         if not auth_header or not auth_header.startswith("Bearer "):
-            return response_error("missing or invalid authorization header", 401, status="error")
+            raise response_error("missing or invalid authorization header", 401, status="error")
 
         token = auth_header.split(" ")[1]
         try:
             payload = jwt.decode(token, settings.jwt_token.SECRET_KEY, algorithms=[settings.jwt_token.ALGORITHM])
+            logging.info(f"payload: {payload}")
             user_id = payload.get("sub")
+            logging.info(f"user_id: {user_id}")
             if user_id is None:
-                return response_error("invalid token payload", 401, status="error")
+                raise response_error("invalid token payload", 401, status="error")
         except jwt.PyJWTError:
-            return response_error("invalid token", 401, status="error")
-
-        response = await call_next(request)
-        return response
+            raise response_error("invalid token", 401, status="error")
