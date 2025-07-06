2ee035fa92a27c9a9906650f1d7aed8881ad7eb6
Siarhei Samoshyn <gds.grey@gmail.com>
2025-07-06 13:27:42 +0300

    update endpoints

---
+++ app/api/endpoints/point/__init__.py
@@ -0,0 +1,19 @@
+from app.api.endpoints.point.analytics import router as analytics_router
+from app.api.endpoints.point.category import router as category_router
+from app.api.endpoints.point.check_list import router as check_list_router
+from app.api.endpoints.point.client import router as client_router
+from app.api.endpoints.point.coffee_shop import router as coffee_shop_router
+from app.api.endpoints.point.employee import router as employee_router
+from app.api.endpoints.point.employee_shift import router as employee_shift_router
+from app.api.endpoints.point.files import router as files_router
+from app.api.endpoints.point.item import router as item_router
+from app.api.endpoints.point.order import router as order_router
+from app.api.endpoints.point.product import router as product_router
+from app.api.endpoints.point.product_order import router as product_order_router
+from app.api.endpoints.point.recipe_item import router as recipe_item_router
+from app.api.endpoints.point.shift import router as shift_router
+from app.api.endpoints.point.store_item import router as store_item_router
+from app.api.endpoints.point.supplier import router as supplier_router
+from app.api.endpoints.point.supply import router as supply_router
+
+
